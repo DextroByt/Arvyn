3cc9430a0409db44aa950b96296fa98a723ca220
Akash Chakresh <Mr.AkashChakresh@gmail.com>
2025-12-17 19:23:59 +0530

    Delete arvyn-server directory

--- arvyn-server/launch_arvyn_chrome.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting ISOLATED Chrome instance for Arvyn...
-echo This ensures Port 9222 is available even if other Chromes are open.
-start "" "chrome.exe" --remote-debugging-port=9222 --user-data-dir="%TEMP%\arvyn-debug-profile" --no-first-run --no-default-browser-check
-echo Chrome launched. Please verify a new window appeared.
-pause
